019b1cd7f0d1ddfd6541f4a0b7fc2f1a7eceb862
Ratha <longratha.num@gmail.com>
2025-01-16 14:55:42 +0700

    Update public IP

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,13 +62,13 @@ export default {
       process.env.NODE_ENV === "production"
         ? false
         : process.env.NODE_ENV !== "staging",
-    baseURL: process.env.BASE_URL || "http://localhost:8080",
+    baseURL: process.env.BASE_URL || "http://84.247.147.88:8080",
     // proxyHeaders: false,
     // credentials: false
   },
   proxy: {
     "/api/v1/": {
-      target: "http://localhost:8080/api/v1",
+      target: "http://84.247.147.88:8080/api/v1",
       pathRewrite: { "^/api/v1/": "" },
     },
   },
@@ -154,7 +154,7 @@ export default {
   },
   loading: false,
   publicRuntimeConfig: {
-    baseURL: process.env.BASE_URL || "http://localhost:8080",
+    baseURL: process.env.BASE_URL || "http://84.247.147.88:8080",
     nodeEnv: process.env.NODE_ENV || "development",
   },
 };
